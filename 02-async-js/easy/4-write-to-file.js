d21121d9cf494acaefc8280d6b3e91304f72f840
tusharrverma <tusharrverma16@gmail.com>
2023-06-16 00:53:18 +0530

    Wrote and test js code

--- 02-async-js/easy/4-write-to-file.js
+++ 02-async-js/easy/4-write-to-file.js
@@ -1,3 +1,14 @@
 // Write to a file
 // Using the fs library again, try to write to the contents of a file.
 // You can use the fs library to as a black box, the goal is to understand async tasks.
+
+var fs = require('fs');
+
+var data = "This is what the file contains!";
+
+fs.writeFile('abc.txt', data, function(err) {
+    console.log(fs.readFileSync("abc.txt","utf8"));
+});
+
+console.log("hey!");
+
