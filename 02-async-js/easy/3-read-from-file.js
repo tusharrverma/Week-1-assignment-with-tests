9a4694d25ae06a261685c0f9a7da4f0e2d52777d
tusharrverma <tusharrverma16@gmail.com>
2023-06-16 00:34:13 +0530

    Wrote and tested js code

--- 02-async-js/easy/3-read-from-file.js
+++ 02-async-js/easy/3-read-from-file.js
@@ -5,3 +5,14 @@
 // Try to do an expensive operation below the file read and see how it affects the output. 
 // Make the expensive operation more and more expensive and see how it affects the output. 
 
+var fs = require('fs');
+
+fs.readFile('2-counter.js','utf8',function(err, data) {
+    console.log(data);
+})
+
+let sum = 0;
+for (i = 0; i < 10000000000; i++) {
+    sum += i;
+}
+console.log(sum);
