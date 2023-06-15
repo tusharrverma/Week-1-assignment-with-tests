c15645502db21ee82e93883384ef72453df62270
tusharrverma <tusharrverma16@gmail.com>
2023-06-16 02:55:18 +0530

    Wrote and tested js code

--- 02-async-js/medium/1-file-cleaner.js
+++ 02-async-js/medium/1-file-cleaner.js
@@ -1,13 +1,34 @@
-## File cleaner
-Read a file, remove all the extra spaces and write it back to the same file.
+// File cleaner
+// Read a file, remove all the extra spaces and write it back to the same file.
 
-For example, if the file input was
-```
-hello     world    my    name   is       raman
-```
+// For example, if the file input was
+// ```
+// hello     world    my    name   is       raman
+// ```
 
-After the program runs, the output should be
+// After the program runs, the output should be
 
-```
-hello world my name is raman
-```
+// ```
+// hello world my name is raman
+// ```
+
+var fs = require('fs');
+let str;
+fs.readFile('abc.txt', 'utf8', function(err, data) {
+    let dataArray = data.split('');
+    while (dataArray.includes(' ')) {
+        let index = dataArray.indexOf(' ');
+        if (dataArray.at(index-1) === ' ' || dataArray.at(index+1) === ' ') {
+            dataArray.splice(index,1);
+            console.log(dataArray);
+        }
+        else {
+            dataArray.fill(',',index,index+1);
+        }
+    }
+    str = dataArray.toString().replaceAll(',,,',' ').replaceAll(',','');
+
+    fs.writeFile('abc.txt', str, function(err) {
+        console.log(fs.readFileSync('abc.txt','utf8'));
+    });
+});
